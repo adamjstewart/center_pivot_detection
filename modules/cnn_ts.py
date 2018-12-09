5f0696cdcc02f71b783a184e04940ae3bc090ae0
Haroun Habeebe <haroun7@gmail.com>
2018-12-09 01:11:14 -0600

    We got T=12 instead of T=10... made changes to arch

--- modules/cnn_ts.py
+++ modules/cnn_ts.py
@@ -31,25 +31,25 @@ from modules.unet import (
 
 
 class CNNTS3D_add(nn.Module):
-    def __init__(self, args, loadable_state_dict=None, inC=6, inT=10, embedding_size=16):
-        # assert(inT == 10, "Adam, you said I'd get 10 time steps. No more, no less.")
+    def __init__(self, args, loadable_state_dict=None, inC=6, inT=12, embedding_size=12):
+        # assert(inT == 12, "Adam, you said I'd get 12 time steps. No more, no less.")
         super(CNNTS3D_add, self).__init__()
         self.args = args
         self.inC, self.inT = inC, inT
         self.embedding_size = embedding_size
         # Temporal Conv
-        self.temporal_embedder = nn.Sequential(  # T = 10
-            nn.Conv2d(inC, 16, (3, 1), stride=(1, 1), padding=(1, 0)),  # T = 10
+        self.temporal_embedder = nn.Sequential(  # T = 12
+            nn.Conv2d(inC, 16, (3, 1), stride=(1, 1), padding=(1, 0)),  # T = 12
             nn.LeakyReLU(),
             nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 5
+            nn.Conv2d(16, 32, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 6
             nn.LeakyReLU(),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, embedding_size, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 2
+            nn.Conv2d(32, embedding_size, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 3
             nn.LeakyReLU(),
         )
         # Spatial
-        self.unet_add = UNet_add(args, in_channels=2 * embedding_size)
+        self.unet_add = UNet_add(args, in_channels=3 * embedding_size)
 
 
         if loadable_state_dict:
@@ -61,30 +61,30 @@ class CNNTS3D_add(nn.Module):
         N, C, T, H, W = (x.shape)
         x = x.contiguous().view(N, C, T, H*W)
         x = self.temporal_embedder(x)  # N * embedding_size * 1 * HW
-        x = x.view(N, self.embedding_size * 2, 1, H, W).view(N, -1, H, W)
+        x = x.view(N, self.embedding_size * 3, 1, H, W).view(N, -1, H, W)
         return self.unet_add(x)
 
 
 class CNNTS3D_cat(nn.Module):
-    def __init__(self, args, loadable_state_dict=None, inC=6, inT=10, embedding_size=16):
-        # assert(inT == 10, "Adam, you said I'd get 10 time steps. No more, no less.")
+    def __init__(self, args, loadable_state_dict=None, inC=6, inT=12, embedding_size=16):
+        # assert(inT == 12, "Adam, you said I'd get 12 time steps. No more, no less.")
         super(CNNTS3D_cat, self).__init__()
         self.args = args
         self.inC, self.inT = inC, inT
         self.embedding_size = embedding_size
         # Temporal Conv
-        self.temporal_embedder = nn.Sequential(  # T = 10
-            nn.Conv2d(inC, 16, (3, 1), stride=(1, 1), padding=(1, 0)),  # T = 10
+        self.temporal_embedder = nn.Sequential(  # T = 12
+            nn.Conv2d(inC, 16, (3, 1), stride=(1, 1), padding=(1, 0)),  # T = 12
             nn.LeakyReLU(),
             nn.BatchNorm2d(16),
             nn.Conv2d(16, 32, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 5
             nn.LeakyReLU(),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, embedding_size, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 2
+            nn.Conv2d(32, embedding_size, (4, 1), stride=(2, 1), padding=(1, 0)),  # T = 3
             nn.LeakyReLU(),
         )
         # Spatial
-        self.unet_cat = UNet_cat(args, in_channels=2 * embedding_size)
+        self.unet_cat = UNet_cat(args, in_channels=3 * embedding_size)
 
 
         if loadable_state_dict:
@@ -96,13 +96,13 @@ class CNNTS3D_cat(nn.Module):
         N, C, T, H, W = (x.shape)
         x = x.contiguous().view(N, C, T, H*W)
         x = self.temporal_embedder(x)  # N * embedding_size * 1 * HW
-        x = x.view(N, self.embedding_size * 2, 1, H, W).view(N, -1, H, W)
+        x = x.view(N, self.embedding_size * 3, 1, H, W).view(N, -1, H, W)
         return self.unet_cat(x)
 
 
 
 class CNNTS2D_add(nn.Module):
-    def __init__(self, args, loadable_state_dict=None, inC=6, inT=10):
+    def __init__(self, args, loadable_state_dict=None, inC=6, inT=12):
         super(CNNTS2D_add, self).__init__()
         self.args = args
         self.inC, self.inT = inC, inT
@@ -117,7 +117,7 @@ class CNNTS2D_add(nn.Module):
         return self.unet_add(x)
 
 class CNNTS2D_cat(nn.Module):
-    def __init__(self, args, loadable_state_dict=None, inC=6, inT=10):
+    def __init__(self, args, loadable_state_dict=None, inC=6, inT=12):
         super(CNNTS2D_cat, self).__init__()
         self.args = args
         self.inC, self.inT = inC, inT
