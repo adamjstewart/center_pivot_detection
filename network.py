0edaad6833a49f9453e3b08941bd35cc31972dfd
Vishal Jagannath Ravi <vishalrjagan@gmail.com>
2018-12-09 21:23:32 -0600

    Add files via upload

--- network.py
+++ network.py
@@ -17,10 +17,10 @@ class Network:
         self.sess.run(self.init)
         self.train()
     def variables(self):
-        self.x = tf.placeholder(tf.float64, shape=(None, self.train_X.shape[1]*self.train_X.shape[2]*self.train_X.shape[3]))
-        self.y = tf.placeholder(tf.bool, shape=(None, self.train_Y.shape[1]*self.train_Y.shape[2]))
-        self.w = tf.Variable(xavier_init(self.train_X.shape[1]*self.train_X.shape[2]*self.train_X.shape[3],self.train_Y.shape[1]*self.train_Y.shape[2]), name="weight")
-        self.b = tf.Variable(tf.zeros([self.train_Y.shape[1]*self.train_Y.shape[2]]), dtype = tf.float64)
+        self.x = tf.placeholder(tf.float64, shape=(None, self.train_X.shape[1]))
+        self.y = tf.placeholder(tf.bool, shape=(None, self.train_Y.shape[1]))
+        self.w = tf.Variable(xavier_init(self.train_X.shape[1],self.train_Y.shape[1]), name="weight")
+        self.b = tf.Variable(tf.zeros([self.train_Y.shape[1]]), dtype = tf.float64)
     def network(self, regParam = 0.1, lrate = 0.001):
         self.output = tf.add(tf.matmul(self.x, self.w),self.b)
         loss = -tf.reduce_sum(self.y * tf.log(1e-6 + self.output) + (1-self.y) * tf.log(1e-6 + 1 - self.output))
@@ -33,6 +33,7 @@ class Network:
             for i in range(train_X.size/batch_size):
                 opt, cost = self.sess.run((self.optimizer, self.cost), feed_dict={self.x: self.train_X[i*batch_size:(i+1)*batch_size,:], self.y: self.train_Y[i*batch_size:(i+1)*batch_size,:]})
                 tot_cost+=cost
+            print("Epochs: ",j, "\nTotal Cost: ", tot_cost)
         return tot_cost
     def test(self, X):
         return self.sess.run(self.output, feed_dict={self.x: X})
