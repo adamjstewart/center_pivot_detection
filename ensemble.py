73b71b5c7ea8e8ac70bb649ff1bbd4ae2f63cfb0
Vishal Jagannath Ravi <vishalrjagan@gmail.com>
2018-12-10 05:37:21 -0600

    Add files via upload

--- ensemble.py
+++ ensemble.py
@@ -6,7 +6,7 @@ gdal.UseExceptions()
 import argparse
 import imageio
 from hough import hough
-from elsd import elsd
+# from elsd import elsd
 from datasets import landsat
 from network import Network
 data_folder = 'data'
@@ -17,30 +17,30 @@ print("Training Data Loaded")
 thresholds = list(np.linspace(0.5, 0.9, 3))
 data, _, _, _, _ = train_dataset[0]
 data_net_hough = np.zeros((len(train_dataset), data.shape[1], data.shape[2], data.shape[3]), dtype=np.float32)
-data_net_elsd = np.zeros((len(train_dataset), data.shape[1], data.shape[2], data.shape[3]), dtype=np.float32)
+# data_net_elsd = np.zeros((len(train_dataset), data.shape[1], data.shape[2], data.shape[3]), dtype=np.float32)
 target_net = np.zeros((len(train_dataset), data.shape[2], data.shape[3]))
 print(len(train_dataset))
 for i in range(len(train_dataset)):
     data, target, t, y, x = train_dataset[i]
     preds_hough = np.zeros((data.shape[1],data.shape[2],data.shape[3]))
-    preds_elsd = np.zeros((data.shape[1],data.shape[2],data.shape[3]))
+    # preds_elsd = np.zeros((data.shape[1],data.shape[2],data.shape[3]))
     for j in range(data.shape[1]):
         preds_hough[j,:,:] = (hough(data[:,j,:,:], thresholds))
         # preds_elsd[j,:,:] = (elsd(data[:,j,:,:], thresholds))
     data_net_hough[i,:,:,:] = preds_hough
     # data_net_elsd[i,:,:,:] = preds_elsd
     target_net[i,:,:][target>0] = 1
-    print(i)
+print("Training Network")
 net1 = Network(data_net_hough, target_net) # Training
-net2 = Network(data_net_elsd, target_net)
+# net2 = Network(data_net_elsd, target_net)
 
 # val_dataset = landsat.TimeSeries(subset='val', root=data_folder, pivots=os.path.join('data/u/sciteam/stewart1/center_pivot_detection/data','pivots_2005_utm14_{:03d}{:03d}_clipped.tif'))
 test_dataset = landsat.TimeSeries(subset='test', root=data_folder, pivots=os.path.join('data/u/sciteam/stewart1/center_pivot_detection/data','pivots_2005_utm14_{:03d}{:03d}_clipped.tif'))
 # all_dataset = landsat.TimeSeries(subset='all', root=data_folder, pivots=os.path.join('data/u/sciteam/stewart1/center_pivot_detection/data',pivots_2005_utm14_{:03d}{:03d}_clipped.tif'))
-print("Data Loaded")
+print("Test Data Loaded")
 data, _, _, _, _ = test_dataset[0]
 test_data_net_hough = np.zeros((len(test_dataset), data.shape[1], data.shape[2], data.shape[3]))
-test_data_net_elsd = np.zeros((len(test_dataset), data.shape[1], data.shape[2], data.shape[3]))
+# test_data_net_elsd = np.zeros((len(test_dataset), data.shape[1], data.shape[2], data.shape[3]))
 test_target_net = np.zeros((len(test_dataset), data.shape[2], data.shape[3]))
 
 print(len(test_dataset))
@@ -48,19 +48,22 @@ accuracies = np.zeros(len(test_dataset))
 for i in range(len(test_dataset)):
     data, target, t, y, x = test_dataset[i]
     preds_hough = np.zeros((data.shape[1],data.shape[2],data.shape[3]), dtype=np.float32)
-    preds_elsd = np.zeros((data.shape[1],data.shape[2],data.shape[3]), dtype=np.float32)
+    # preds_elsd = np.zeros((data.shape[1],data.shape[2],data.shape[3]), dtype=np.float32)
     for j in range(data.shape[1]):
         preds_hough[j,:,:] = (hough(data[:,j,:,:], thresholds))
         # preds_elsd[j,:,:] = (elsd(data[j,:,:,:], thresholds))
     pred_max_hough = preds_hough.max(axis=0) # Max pooling
     # pred_max_elsd = preds_elsd.max(axis=0)
-    print(np.mean(pred_max_elsd==target))
-    accuracies[i] = np.mean(pred_max_elsd==target)
+    # print(np.mean(pred_max_elsd==target))
+    accuracies[i] = np.mean(pred_max_hough==(target>0))
     test_data_net_hough[i,:,:,:] = preds_hough
-    test_data_net_elsd[i,:,:,:] = preds_elsd
-    test_target_net[i,:,:][target>0] = 1
+    # test_data_net_elsd[i,:,:,:] = preds_elsd
+    test_target_net[i,:,:] = target>0
 print(np.mean(accuracies))
-pred_fnn_hough = Network.score(test_data_net_hough, test_target_net) # Fully Connected Network
-pred_fnn_elsd = Network.score(test_data_net_elsd, test_target_net)
-print(np.mean(pred_fnn_elsd==test_target_net))
-print(np.mean(pred_fnn_hough==test_target_net))
+pred_fnn_hough = net1.test(test_data_net_hough) # Fully Connected Network
+pred_fnn_hough = np.swapaxes(pred_fnn_hough.reshape(test_target_net.shape), 1, 2)
+# pred_fnn_elsd = net2.test(test_data_net_elsd)
+# print(np.mean(pred_fnn_elsd==test_target_net))
+thresholds = list(np.linspace(0.1, 0.9, 10))
+for thr in thresholds:
+    print(thr, np.mean((pred_fnn_hough>thr)==test_target_net))
--- network.py
+++ network.py
@@ -2,38 +2,36 @@ import tensorflow as tf
 import numpy as np
 
 def xavier_init(fan_in, fan_out):
+    print(fan_in, fan_out)
     high = np.sqrt(6.0/(fan_in+fan_out))
     low = -high
     return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
 
 class Network:
-    def __init__(self, X, y):
-        self.train_X = X.reshape((X.shape[0],X.shape[1]*X.shape[2]*X.shape[3]))
-        self.train_Y = y.reshape((y.shape[0],y.shape[1]*y.shape[2]))
-        self.variables()
-        self.network()
-        self.init = tf.global_variables_initializer()
-        self.sess = tf.Session()
-        self.sess.run(self.init)
-        self.train()
-    def variables(self):
+    def __init__(self, X, y, regParam = 0.2, lrate = 0.001):
+        self.train_X = np.swapaxes(X,1,3).reshape((X.shape[0]*X.shape[3]*X.shape[2],X.shape[1]))
+        self.train_Y = np.swapaxes(y,1,2).reshape((y.shape[0]*y.shape[1]*y.shape[2],1))
         self.x = tf.placeholder(tf.float32, shape=(None, self.train_X.shape[1]))
         self.y = tf.placeholder(tf.float32, shape=(None, self.train_Y.shape[1]))
-        self.w = tf.Variable(xavier_init(self.train_X.shape[1],self.train_Y.shape[1]), name="weight")
-        self.b = tf.Variable(tf.zeros([self.train_Y.shape[1]]), dtype = tf.float32)
-    def network(self, regParam = 0.1, lrate = 0.001):
-        self.output = tf.add(tf.matmul(self.x, self.w),self.b)
+        self.w = tf.Variable(xavier_init(self.train_X.shape[1],self.train_Y.shape[1]), name="w1")
+        self.b = tf.Variable(tf.zeros([self.train_Y.shape[1]]), dtype = tf.float32, name="b1")
+        self.output = tf.nn.sigmoid(tf.add(tf.matmul(self.x, self.w),self.b))
         loss = -tf.reduce_sum(self.y * tf.log(1e-6 + self.output) + (1-self.y) * tf.log(1e-6 + 1 - self.output))
         regularizer = tf.nn.l2_loss(self.w)
         self.cost = tf.reduce_mean(loss) + regParam*regularizer
         self.optimizer = tf.train.AdamOptimizer(learning_rate = lrate).minimize(self.cost)
-    def train(self, batch_size=200, epochs=100):
+        self.init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(self.init)
+        self.train()
+    def train(self, batch_size=128*128, epochs=50):
+        print("Beginning Training")
         for j in range(epochs):
             tot_cost = 0
-            for i in range(int(train_X.size/batch_size)):
+            for i in range(int(self.train_X.shape[0]/batch_size)):
                 opt, cost = self.sess.run((self.optimizer, self.cost), feed_dict={self.x: self.train_X[i*batch_size:(i+1)*batch_size,:], self.y: self.train_Y[i*batch_size:(i+1)*batch_size,:]})
                 tot_cost+=cost
             print("Epochs: ",j, " Total Cost: ", tot_cost)
         return tot_cost
     def test(self, X):
-        return self.sess.run(self.output, feed_dict={self.x: X})
+        return self.sess.run(self.output, feed_dict={self.x: np.swapaxes(X,1,3).reshape((X.shape[0]*X.shape[3]*X.shape[2],X.shape[1]))})
